d0ab4aeed3a581cd4542e28cd94a957b073e6e5e
ujihisa <ujihisa@gmail.com>
2018-09-16 16:01:15 +0900

    ひどいコード

--- webapp/ruby/lib/torb/web.rb
+++ webapp/ruby/lib/torb/web.rb
@@ -61,9 +61,9 @@ module Torb
         t = Time.now
         db.query('BEGIN')
         begin
-          event_ids = db.query('SELECT * FROM events ORDER BY id ASC').select(&where).map { |e| e['id'] }
-          events = event_ids.map do |event_id|
-            event = get_event(event_id)
+          events0 = db.query('SELECT * FROM events ORDER BY id ASC').select(&where)
+          events = events0.map do |event0|
+            event = get_event_sat0yu(event0.id, event0)
             event['sheets'].each { |sheet| sheet.delete('detail') }
             event
           end
@@ -77,6 +77,46 @@ module Torb
         events
       end
 
+      def get_event_sat0yu(event_id, event)
+        # event = db.xquery('SELECT * FROM events WHERE id = ?', event_id).first
+        # return unless event
+
+        # zero fill
+        event['total']   = 0
+        event['remains'] = 0
+        event['sheets'] = {}
+        %w[S A B C].each do |rank|
+          event['sheets'][rank] = { 'total' => 0, 'remains' => 0, 'detail' => [] }
+        end
+
+        sheets = db.query('SELECT * FROM sheets ORDER BY `rank`, num')
+        sheets.each do |sheet|
+          event['sheets'][sheet['rank']]['price'] ||= event['price'] + sheet['price']
+          event['total'] += 1
+          event['sheets'][sheet['rank']]['total'] += 1
+
+          reservation = db.xquery('SELECT * FROM reservations WHERE event_id = ? AND sheet_id = ? AND canceled_at IS NULL GROUP BY event_id, sheet_id HAVING reserved_at = MIN(reserved_at)', event['id'], sheet['id']).first
+          if reservation
+            sheet['mine']        = true if login_user_id && reservation['user_id'] == login_user_id
+            sheet['reserved']    = true
+            sheet['reserved_at'] = reservation['reserved_at'].to_i
+          else
+            event['remains'] += 1
+            event['sheets'][sheet['rank']]['remains'] += 1
+          end
+
+          event['sheets'][sheet['rank']]['detail'].push(sheet)
+
+          sheet.delete('id')
+          sheet.delete('price')
+          sheet.delete('rank')
+        end
+
+        event['public'] = event.delete('public_fg')
+        event['closed'] = event.delete('closed_fg')
+
+        event
+      end
       def get_event(event_id, login_user_id = nil)
         event = db.xquery('SELECT * FROM events WHERE id = ?', event_id).first
         return unless event
