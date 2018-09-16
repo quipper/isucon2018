68814356fef26a7501a834f672a9fcbf9c5e9b4f
ujihisa <ujihisa@gmail.com>
2018-09-16 16:45:43 +0900

    あかんわ

--- webapp/ruby/lib/torb/web.rb
+++ webapp/ruby/lib/torb/web.rb
@@ -73,7 +73,8 @@ module Torb
           $logger.info("sheets.first: #{sheets.first}")
 
           events = event_ids.map do |event_id|
-            event = get_event(event_id, nil, raw_event_hash[event_id], sheets.clone)
+            # event = get_event(event_id, nil, raw_event_hash[event_id], sheets.clone)
+            event = get_event(event_id, nil, raw_event_hash[event_id])
             event['sheets'].each { |sheet| sheet.delete('detail') }
             event
           end
