40e5a62222e85cfac6f263f2f17c1bc477def9c7
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-16 16:15:28 +0900

    めっちゃ許可する

--- db/init-user.sh
+++ db/init-user.sh
@@ -3,6 +3,6 @@
 cat <<'EOF' | mysql -uroot
 CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
 GRANT ALL ON torb.* TO 'isucon'@'%';
-CREATE USER 'isucon'@'localhost' IDENTIFIED BY 'isucon';
-GRANT ALL ON torb.* TO 'isucon'@'localhost';
+CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
+GRANT ALL ON torb.* TO 'isucon'@'%';
 EOF
